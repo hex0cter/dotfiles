2ce1c84fc546c9c6b6bdf0984ac98ce297404ddb
Daniel Han <daniel.han@scrive.com>
2021-01-07 20:37:23 +0100

    Update the installation script

--- .install.sh
+++ .install.sh
@@ -1,11 +1,34 @@
 #!/usr/bin/env bash
 
+install_dotfiles() {
+  git clone --bare git@github.com:hex0cter/dotfiles.git $HOME/.cfg
+
+  alias dot='git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
+  mkdir -p .config-backup
+  dot checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+}
+
+install_zsh() {
+  sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+}
+
 install_linux() {
+  sudo apt update
+  sudo apt -y install git tmux zsh
   echo "install linux"
+
+  install_zsh
+  install_dotfiles
 }
 
 install_macos() {
+  brew update
+  brew install git tmux zsh
   echo "install macos"
+
+  install_zsh
+  install_dotfiles
 }
 
 os=$(uname)
