57a4ee821639492decc78bbf1e54f38392d330c9
Daniel Han <daniel.han@scrive.com>
2021-01-08 10:57:00 +0100

    Update the installation script

--- .install.sh
+++ .install.sh
@@ -68,7 +68,7 @@ install_zsh() {
 
 install_linux() {
   sudo apt update
-  sudo apt -y install git tmux zsh golang python-is-python2 python3
+  sudo apt -y install git tmux zsh golang python-is-python2 python3 fdclone
 
   install_dotfiles
   install_zsh
