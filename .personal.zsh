68cd3e299d63cce3c59ed013837fd04a17a8e112
Daniel Han <daniel.han@scrive.com>
2021-02-01 13:34:04 +0100

    bugfix for cd

--- .personal.zsh
+++ .personal.zsh
@@ -43,7 +43,9 @@ t() {
 }
 
 cd() {
-   if [ "$1" != "" ]; then
+   if [ "$1" = "-" ]; then
+     builtin cd -
+   elif [ "$1" != "" ]; then
      builtin cd $(realpath $1)
    else
      builtin cd
@@ -83,6 +85,7 @@ alias wiki='wikit' # npm i -g wikit
 alias weat='curl http://wttr.in/'
 alias icat="kitty +kitten icat"
 alias dot="git --git-dir=${HOME}/.cfg/ --work-tree=${HOME}"
+alias zip-with-password='zip -er'
 
 export ANDROID_HOME=$HOME/Library/Android/sdk
 export PATH=$PATH:$ANDROID_HOME/platform-tools:$ANDROID_HOME/emulator
